6463ecae58f652e46fc01fee67273c81a63f4fa4
m-aljamal <mohammadjamol@gmail.com>
2022-05-30 10:25:34 +0300

    edit map

--- src/components/contact/Loaction.js
+++ src/components/contact/Loaction.js
@@ -2,11 +2,11 @@ import React from "react"
 import Title from "../shared/Title"
 const Loaction = () => {
   return (
-    <div className='my-10'>
+    <div className="my-10">
       <Title text="موقعنا:" />
       <div className="  container ">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6396.871502089596!2d37.132061000000014!3d36.7120947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2str!4v1652337861180!5m2!1sen!2str"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1599.2115689049206!2d37.131199358721915!3d36.712397700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x152fd1e4b45da09d%3A0x83ba18437effb9d9!2zRG_En2FuIEfDvHJlxZ8gUGHFn2EsIE11c3RhZmEgRXJlbiBDZCBObzo0LCA3OTAwMCBLaWxpcyBNZXJrZXovS2lsaXM!5e0!3m2!1sen!2str!4v1653895452107!5m2!1sen!2str"
           width="100%"
           title="EDC_location"
           height="350"
@@ -20,5 +20,3 @@ const Loaction = () => {
 }
 
 export default Loaction
-
-
