229c976e7d67218615079a6e44684ca8da8c6e16
m-aljamal <mohammadjamol@gmail.com>
2022-05-30 16:00:13 +0300

    add report

--- src/components/home/Reports.js
+++ src/components/home/Reports.js
@@ -7,14 +7,16 @@ const Reports = ({ lang }) => {
       <Title title={lang === "ar" ? "التقارير" : "Reports"} />
       <div className="grid grid-cols-3 gap-8 mt-8">
         <a
-          className=" shadow-md rounded-md"
           target="_blank"
           rel="noopener noreferrer"
           href="https://drive.google.com/file/d/1YTSB1qJha0FDiP1dmqta-gcuM60y_Le9/view?usp=sharing"
         >
           <img src={edc2021} className="w-full shadow-md rounded-md " />
+          <p className="text-mainblue font-medium text-center pt-2">
+            اضغط لقراة التقرير
+          </p>
         </a>
-        <a
+        {/* <a
           target="_blank"
           rel="noopener noreferrer"
           href="https://drive.google.com/file/d/1YTSB1qJha0FDiP1dmqta-gcuM60y_Le9/view?usp=sharing"
@@ -27,7 +29,7 @@ const Reports = ({ lang }) => {
           href="https://drive.google.com/file/d/1YTSB1qJha0FDiP1dmqta-gcuM60y_Le9/view?usp=sharing"
         >
           <img src={edc2021} className="w-full shadow-md rounded-md" />
-        </a>
+        </a> */}
       </div>
     </div>
   )
